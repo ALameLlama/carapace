9b31f327b79326b22b92757d2828df5251b0904c
Nicholas Ciechanowski <nicholas@ciech.anow.ski>
2025-08-25 18:23:29 +1000

    feat: add `GroupFrom` Attribute for grouping flat keys into nested properties

---
+++ src/Attributes/GroupFrom.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Alamellama\Carapace\Attributes;
+
+use Alamellama\Carapace\Contracts\PropertyPreHydrationInterface;
+use Alamellama\Carapace\Support\Data;
+use Attribute;
+use ReflectionProperty;
+
+/**
+ * Groups multiple flat input keys into a nested structure for a property.
+ *
+ * Example:
+ * #[GroupFrom('street', 'city', 'postcode')]
+ * public Address $address;
+ *
+ * If the incoming data has keys `street`, `city`, and `postcode`, this attribute
+ * will collect them into an array and assign it to the `address` property, allowing
+ * ImmutableDTO to hydrate the nested Address DTO automatically.
+ */
+#[Attribute(Attribute::TARGET_PROPERTY)]
+final class GroupFrom implements PropertyPreHydrationInterface
+{
+    /**
+     * @var array<string>
+     */
+    public array $sourceKeys;
+
+    public function __construct(string ...$sourceKeys)
+    {
+        $this->sourceKeys = $sourceKeys;
+    }
+
+    public function propertyPreHydrate(ReflectionProperty $property, Data $data): void
+    {
+        $propertyName = $property->getName();
+
+        // Do not override if the property value is already present.
+        if ($data->has($propertyName)) {
+            return;
+        }
+
+        $group = [];
+
+        foreach ($this->sourceKeys as $key) {
+            if (! $data->has($key)) {
+                continue;
+            }
+
+            $group[$key] = $data->get($key);
+            $data->unset($key);
+        }
+
+        if ($group !== []) {
+            $data->set($propertyName, $group);
+        }
+    }
+}
